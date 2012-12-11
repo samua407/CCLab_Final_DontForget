67fcee1515a1884a71e27d47bdc20b57f0c790fb
samua407 <samua407@newschool.edu>
2012-12-11 16:35:00 -0500

    intial file

---
+++ Final_Arduino.ino
@@ -0,0 +1,6 @@
+const int ledUmbrella =  13;      // the number of the LED pin
+const int ledSunglasses =  13;      // the number of the LED pin
+const int ledMittens =  13;      // the number of the LED pin
+const int ledTissues =  13;      // the number of the LED pin
+const int ledPower =  13;      // the number of the LED pin
+
