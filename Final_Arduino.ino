7fb0f02d8de4af420e57fd3d7bcc7aec379bc91e
samua407 <samua407@newschool.edu>
2012-12-13 17:22:20 -0500

    LED + Temp, Humid, Sun Sensors

--- Final_Arduino.ino
+++ Final_Arduino.ino
@@ -1,9 +1,22 @@
-//const pin number
-int ledUmbrella =  8;      // the number of the LED pin
-int ledSunglasses = 9;      // the number of the LED pin
-int ledMittens =  10;      // the number of the LED pin
-int ledTissues =  11;      // the number of the LED pin
-int ledPower =  7;      // the number of the LED pin
+//LED Pins
+int ledUmbrella =  11;      // the number of the LED pin
+int ledSunglasses = 10;      // the number of the LED pin
+int ledMittens =  9;      // the number of the LED pin
+int ledTissues =  8;      // the number of the LED pin
+int ledPower =  12;      // the number of the LED pin
+
+//SENSOR INT - TEMP
+int sensorPinTemp = A0;
+
+//SENSOR INT - SUNNIES
+int sensorPinSunnies = A2;      
+int sunniesValue = 0;  
+int newSunniesValue;
+
+//SENSOR INT - HUMID
+float sensorPinHumid = A1;
+float humidityValue = 0;
+float relHumid = 0;
 
 
 
@@ -22,45 +35,76 @@ void setup() {
 void loop(){
 
 
-  /////Power Output///////////////////////////////////
+/////Power Output///////////////////////////////////
   digitalWrite(ledPower, HIGH);
-  
-  /////Serial Read///////////////////////////////////
+
+/////Serial Read///////////////////////////////////
   while (Serial.available() == 0);
   int val = Serial.read() - '0';
 
+/////Humidity Input - from http://arduino.cc/forum/index.php/topic,19961.0.html#11///////////////////////////////////
+  humidityValue = analogRead(sensorPinHumid);
+  relHumid = ((((humidityValue/1023)*5)-0.8)/3.1)*100;
+  //Serial.println(relHumid);
 
-  /////Umbrella Output///////////////////////////////////
-  if (val == 0)
+  delay(300);
+
+/////Humidty Output
+  if (relHumid >= 70)
   {
+    Serial.println("Don't forget your umbrella!");
     digitalWrite(ledUmbrella, HIGH);
   }
 
-  if (val == 5)
+  if (relHumid = 4)
   {
+    Serial.println("You don't need an umbrella.");
     digitalWrite(ledUmbrella, LOW);
   }
-
-  /////Sunglasses Output///////////////////////////////////
-  if (val == 1)
+/////Sun Input
+  sunniesValue = analogRead(sensorPinSunnies);
+  newSunniesValue = map(sunniesValue, 0, 1023, 0, 100);
+  //Serial.println(newSunniesValue);
+  
+/////Sun Output
+  if (newSunniesValue >= 20)
   {
+    Serial.println("Don't forget your sunnies!");
     digitalWrite(ledSunglasses, HIGH);
   }
 
-  if (val == 6)
+  if (newSunniesValue <= 10)
   {
+    Serial.println("You don't need your sunnies.");
     digitalWrite(ledSunglasses, LOW);
   }
 
 
-  /////Mittens Output///////////////////////////////////
-  if (val == 2)
+/////Temp Input - from adafruit tutorial
+
+  // converting reading to voltage
+  int readingGloves = analogRead(sensorPinTemp);  
+  float voltageGloves = readingGloves * 5.0;
+  voltageGloves /= 1024.0; 
+
+  //convert to Celsius
+  float temperatureC = (voltageGloves - 0.5) * 100;
+
+  //convert to Fahrenheight
+  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  
+  Serial.println(temperatureF);
+
+/////Temp Output
+  if (temperatureF <= 45) 
   {
+    Serial.println("Don't forget your gloves!"); 
     digitalWrite(ledMittens, HIGH);
   }
 
-  if (val == 7)
+  if (temperatureF >= 46)
   {
+    Serial.println("You don't need gloves.");
     digitalWrite(ledMittens, LOW);
   }
 
@@ -78,6 +122,7 @@ void loop(){
 
 
 
+    Serial.flush();
 
 }
 
