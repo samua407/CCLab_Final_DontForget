75db8fccd220ae1a6c5127f6984a2b2cc6310daf
samua407 <samua407@newschool.edu>
2012-12-15 14:13:30 -0500

    code split to LED (working) + SENSORS (in prog)

---
+++ xbee_led/xbee_led.ino
@@ -0,0 +1,57 @@
+//LED Pins
+int ledUmbrella =  11;      // the number of the LED pin
+int ledSunglasses = 10;      // the number of the LED pin
+int ledMittens =  9;      // the number of the LED pin
+int ledTissues =  8;      // the number of the LED pin
+int ledPower =  7;      // the number of the LED pin
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // set the pin as output:
+  pinMode(ledUmbrella, OUTPUT);      
+  pinMode(ledSunglasses, OUTPUT);      
+  pinMode(ledMittens, OUTPUT);      
+  pinMode(ledTissues, OUTPUT);      
+  pinMode(ledPower, OUTPUT);      
+
+}
+
+void loop(){
+  while(Serial.available()){  //is there anything to read?
+    char getData = Serial.read();  //if yes, read it
+
+    digitalWrite(ledPower, HIGH);
+    
+    if(getData == 'U'){
+      digitalWrite(ledUmbrella, HIGH);
+    }
+    else if(getData == 'u'){
+      digitalWrite(ledUmbrella, LOW);
+    }
+
+    if(getData == 'S'){
+      digitalWrite(ledSunglasses, HIGH);
+    }
+    else if(getData == 's'){
+      digitalWrite(ledSunglasses, LOW);
+    }
+
+    if(getData == 'M'){
+      digitalWrite(ledMittens, HIGH);
+    }
+    else if(getData == 'm'){
+      digitalWrite(ledMittens, LOW);
+    }
+
+    if(getData == 'T'){
+      digitalWrite(ledTissues, HIGH);
+    }
+    else if(getData == 't'){
+      digitalWrite(ledTissues, LOW);
+    }
+  }
+}
+
+
---
+++ xbee_sensor/xbee_sensor.ino
@@ -0,0 +1,108 @@
+
+//SENSOR INT - TEMP
+int sensorPinTemp = A0;
+
+//SENSOR INT - HUMID
+float sensorPinHumid = A1;
+float humidityValue = 0;
+float relHumid = 0;
+
+//SENSOR INT - SUNNIES
+int sensorPinSunnies = A2;      
+int sunniesValue = 0;  
+int newSunniesValue;
+
+
+void setup() {
+  Serial.begin(9600);
+
+
+}
+
+void loop(){
+
+  /////Serial Read///////////////////////////////////
+  while (Serial.available() == 0);
+  int val = Serial.read() - '0';
+
+  /////Humidity Input - from http://arduino.cc/forum/index.php/topic,19961.0.html#11///////////////////////////////////
+  humidityValue = analogRead(sensorPinHumid);
+  relHumid = ((((humidityValue/1023)*5)-0.8)/3.1)*100;
+  //Serial.println(relHumid);
+
+  delay(300);
+
+  /////Humidty Output
+  if (relHumid >= 60)
+  {
+    Serial.println("U"); //need umbrella
+  }
+
+  if (relHumid <= 59)
+  {
+    Serial.println("u"); //no umbrella
+  }
+
+  /////Sun Input
+  sunniesValue = analogRead(sensorPinSunnies);
+  newSunniesValue = map(sunniesValue, 0, 1023, 0, 100);
+  //Serial.println(newSunniesValue);
+
+  /////Sun Output
+  if (newSunniesValue >= 20)
+  {
+    Serial.println("S"); //need sunglasses
+  }
+
+  if (newSunniesValue <= 19)
+  {
+    Serial.println("s"); //no sunglasses
+  }
+
+
+  /////Temp Input - from adafruit tutorial
+
+  // converting reading to voltage
+  int readingGloves = analogRead(sensorPinTemp);  
+  float voltageGloves = readingGloves * 5.0;
+  voltageGloves /= 1024.0; 
+
+  //convert to Celsius
+  float temperatureC = (voltageGloves - 0.5) * 100;
+
+  //convert to Fahrenheight
+  float temperatureF = ((temperatureC * 9.0 / 5.0) + 32.0) * -1;
+
+  Serial.println(temperatureF);
+
+  /////Temp Output
+  if (temperatureF <= 45) 
+  {
+    Serial.println("M"); //need mittens
+  }
+
+  if (temperatureF >= 46)
+  {
+    Serial.println("m"); //no mittens
+  }
+
+
+  /////Tissues Output///////////////////////////////////
+  if (val == 3)
+  {
+    Serial.println("T"); //tissues
+  }
+
+  if (val == 8)
+  {
+    Serial.println("t"); //no tissues
+  }
+
+
+
+  Serial.flush();
+
+}
+
+
+
